53f4288c1eb9daf3f0db480604494846b4615caf
AlePaff <43119891+AlePaff@users.noreply.github.com>
2022-08-12 12:39:41 -0300

    orden tarjetas

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-var fechaActualizacion = "29/06/2022";
+var fechaActualizacion = "12/08/2022";
 
 
 // inicializacion de los tooltip
@@ -24,13 +24,14 @@ function createCard(nombre, desc, link, linkImage) {
 			      </div>
 			    </div>
 			  </div>`
-		}
+		};
 
+// uso de $ y de _ en javascript -> https://www.thoughtco.com/and-in-javascript-2037515
 $(function(){
 			// $('#card_').append($(createCard('preset_nombre', "descripcion", "link", "imagen")));
 
-			$('#card_fiuba_drive').append($(createCard('Fiuba Drive', 	"Drive creado para subir material de tu curso y encontrar el de otros", "https://sites.google.com/view/drive-fiuba/", "images/fiuba-drive.jpg")));
 			$('#card_wiki_fiuba').append($(createCard('Wiki Fiuba', 	"Espacio colaborativo donde podés encontrar materiales de estudio para la carrera", "http://wiki.foros-fiuba.com.ar/", "images/wiki-fiuba.jpg")));
+			$('#card_fiuba_drive').append($(createCard('Fiuba Drive', 	"Drive creado para subir material de tu curso y encontrar el de otros", "https://sites.google.com/view/drive-fiuba/", "images/fiuba-drive.jpg")));
 			$('#card_sk_fiuba').append($(createCard('SK FIUBA',	 		"Guias, examenes y apuntes para las materias de ingeniería industrial", "https://sites.google.com/view/skfiuba/materias?authuser=0", "images/sk-fiuba.jpg")));
 			$('#card_filadd').append($(createCard('Filadd', 			"Distintos recursos de varias materias de la fiuba", "https://filadd.com/resultados-aportes/uba/ingenieria-uba/probabilidad-y-estadistica-a/51/2349", "images/filadd.jpg")));
 			$('#card_js_apuntes').append($(createCard('JS Apuntes', 	"Material dividido por carpetas descargables para materias de ingeniería industrial", "https://sites.google.com/view/site-js-apuntes", "images/js-apuntes.jpg")));
@@ -41,8 +42,8 @@ $(function(){
 			$('#card_fiuba_consultas').append($(createCard('Fiuba Consultas', "Grupo general sobre consultas, temas administrativos, noticias, polemica y memes", "https://www.facebook.com/groups/fiubaconsultas2", "images/fiuba-consultas.jpg")));
 
 
-			$('#card_fiuba_opiniones_compu').append($(createCard('FIUBA Opiniones de Cursos', "Opiniones sobre materias del depto de computación", "https://fiuba-opiniones-de-curso.herokuapp.com/", "images/opiniones-fiuba.jpg")));
 			$('#card_dolly_fiuba').append($(createCard('Dolly Fiuba', "Encuestas de estudiantes sobre cada catedra y materia de la fiuba", "https://dollyfiuba.com/", "images/dolly.jpg")));
+			$('#card_fiuba_opiniones_compu').append($(createCard('FIUBA Opiniones de Cursos', "Opiniones sobre materias del depto de computación", "https://fiuba-opiniones-de-curso.herokuapp.com/", "images/opiniones-fiuba.jpg")));
 
 
 			$('#card_fiuba_map').append($(createCard('Fiuba Map', "Diagrama interactivo de correlatividades de materias de cada carrera", "https://fede.dm/FIUBA-Map/", "images/fiuba-map.jpg")));
