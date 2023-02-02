dbf5ba64fe79c9d6e4d5cad2045a1d5a75bd0cd0
AlePaff <alejandropaff@gmail.com>
2023-02-01 23:03:34 -0300

    base para el footer

--- sabelo-fiuba/src/components/Footer.js
+++ sabelo-fiuba/src/components/Footer.js
@@ -1,13 +1,59 @@
 // 3 iconos 
 // fecha de ultima actualización
+import { Box, IconButton, Text, Tooltip, Flex } from '@chakra-ui/react'
+import { WarningTwoIcon, SearchIcon } from '@chakra-ui/icons'
 
-import { Text } from '@chakra-ui/react'
 
 function Footer(props) {
-    return(
-        <Text>
-            {props.children}
-        </Text>
+    return (
+        <Box
+            backgroundColor="#1b2932"
+            color={"white"}
+            padding={10}
+            textAlign="center"
+            fontSize={"sm"}
+        >
+
+            <Flex gap='2' justifyContent='center'>
+
+
+                {/* icono con tooltip y link a google.com */}
+                <Tooltip label="Buscar en Google" fontSize='md' >
+                    <IconButton
+                        // w={6} h={6} color="red.500"
+                        aria-label="Buscar en Google"
+                        icon={<SearchIcon />}
+                        size="lg"
+                        colorScheme="teal"
+                        variant="outline"
+                        onClick={() => window.open('https://www.google.com', '_blank')}
+                    />
+                </Tooltip>
+
+                <Tooltip label="Buscar en Google" fontSize='md' >
+                    <IconButton
+                        // w={6} h={6} color="red.500"
+                        aria-label="Buscar en Google"
+                        icon={<SearchIcon />}
+                        size="lg"
+                        colorScheme="teal"
+                        variant="outline"
+                        onClick={() => window.open('https://www.google.com', '_blank')}
+                    />
+                </Tooltip>
+
+
+            </Flex>
+
+
+
+            <Text>
+                {props.children}
+            </Text>
+
+        </Box>
+
+
     )
 }
 
