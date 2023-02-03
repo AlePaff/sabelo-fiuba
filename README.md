bf0a5ba63ef1a733e5179ba83b5260b472afe18a
AlePaff <alejandropaff@gmail.com>
2023-02-03 00:16:05 -0300

    cosas viejas elimnadas

--- README.md
+++
@@ -1,7 +0,0 @@
-## [Proyectos de la Fiuba](https://alepaff.github.io/sabelo-fiuba/)
-
-Se trata de una recolección de material de apuntes, resumenes, noticias, grupos y nuevos proyectos relacionados a la Fiuba, intenta unificar todo aquello que está suelto y vale la pena *saber* que existe.
-
-Podés añadir nuevos enviando un formulario al siguiente [link](https://docs.google.com/forms/d/e/1FAIpQLSe9v8y4L8Dtek3y9u8d5uSiB5SK-P-3FUvo1nhOJIF-pD8j3A/viewform)
-
-Cualquier comentario, opinión, nueva sugerencia o bug que encuentres podes enviarmelo por [este google form]( https://docs.google.com/forms/d/e/1FAIpQLScHmIO1xOiU0hGEVrJgEbKwY8YR4RDCv7aPU451oGFxkY_rgg/viewform) o bien creando un nuevo issue
--- sabelo-fiuba/src/components/Footer.js
+++ sabelo-fiuba/src/components/Footer.js
@@ -1,55 +1,100 @@
-// 3 iconos 
-// fecha de ultima actualización
 import { Box, IconButton, Text, Tooltip, Flex } from '@chakra-ui/react'
-import { WarningTwoIcon, SearchIcon } from '@chakra-ui/icons'
+import { useEffect, useState } from 'react'
 
 
-function Footer(props) {
+function Footer() {
+
+    //obtener la fecha de ultima actualización del repo de github
+    const apiUrl = "https://api.github.com/repos/AlePaff/sabelo-fiuba/branches/main"
+    const [showData, setShowData] = useState()      //estado para mostrar la fecha
+    let displayData
+
+    async function pullJson() {
+        const response = await fetch(apiUrl)
+        const responseData = await response.json()
+        // console.log(responseData.commit.commit.author.date)
+        displayData = responseData.commit.commit.author.date
+
+        //transformar displayData en un formato de fecha
+        let dateLastCommit = new Date(responseData.commit.commit.author.date)
+        let day = dateLastCommit.getDate();
+        let month = dateLastCommit.getMonth();
+        let year = dateLastCommit.getFullYear();
+        let formato = day + "/" + month + "/" + year;
+        displayData = formato
+
+        setShowData(displayData)        //actualizar el estado
+    }
+
+    useEffect(() => {
+        pullJson()
+    })
+
     return (
         <Box
             backgroundColor="#1b2932"
             color={"white"}
-            padding={10}
+            padding={"30px 0px 10px 0px"}
             textAlign="center"
             fontSize={"sm"}
         >
 
-            <Flex gap='2' justifyContent='center'>
+            <Flex gap='1' justifyContent='center'>
 
 
-                {/* icono con tooltip y link a google.com */}
-                <Tooltip label="Buscar en Google" fontSize='md' >
+                <Tooltip label="Ir al repositorio" placement='top'>
                     <IconButton
-                        // w={6} h={6} color="red.500"
-                        aria-label="Buscar en Google"
-                        icon={<SearchIcon />}
+                        icon={
+                            <img src="https://github.githubassets.com/favicons/favicon.svg"
+                                alt="github" height="32" width="32"
+                                //invertir colores de la imagen
+                                style={{ filter: 'invert(100%)' }}
+                            />}
                         size="lg"
-                        colorScheme="teal"
-                        variant="outline"
-                        onClick={() => window.open('https://www.google.com', '_blank')}
+                        onClick={() => window.open('https://github.com/AlePaff/sabelo-fiuba', '_blank')}
+                        isRound={true}
+                        _hover={{ backgroundColor: "#2d3748" }}
+                        variant="ghost"     //hace que solo aparezca el icono sin el fondo
+
                     />
                 </Tooltip>
 
-                <Tooltip label="Buscar en Google" fontSize='md' >
+                <Tooltip label="Añadir un nuevo proyecto" placement='top'>
                     <IconButton
-                        // w={6} h={6} color="red.500"
-                        aria-label="Buscar en Google"
-                        icon={<SearchIcon />}
+                        icon={
+                            <img src="https://www.svgrepo.com/show/217145/forms-document.svg"
+                                alt="nuevo proyecto" height="32" width="32" />}
                         size="lg"
+                        onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSe9v8y4L8Dtek3y9u8d5uSiB5SK-P-3FUvo1nhOJIF-pD8j3A/viewform?usp=pp_url&entry.385134802=Otro', '_blank')}
                         colorScheme="teal"
-                        variant="outline"
-                        onClick={() => window.open('https://www.google.com', '_blank')}
+                        isRound={true}
+                        _hover={{ backgroundColor: "#2d3748" }}
+                        variant="ghost"
                     />
                 </Tooltip>
 
+                <Tooltip label="Feedback" placement='top'>
+                    <IconButton
+                        icon={
+                            <img src="https://i.imgur.com/kZ3x8aj.png"
+                                alt="feedback" height="32" width="32" />}
+                        size="lg"
+                        colorScheme="teal"
+                        onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLScHmIO1xOiU0hGEVrJgEbKwY8YR4RDCv7aPU451oGFxkY_rgg/viewform?usp=sf_link', '_blank')}
+                        isRound={true}
+                        _hover={{ backgroundColor: "#2d3748" }}
+                        variant="ghost"
+                    />
+                </Tooltip>
 
             </Flex>
-
-
-
-            <Text>
-                {props.children}
+            
+            <Text fontSize="xs" color="gray.500" mt="2">
+                Última actualización: {showData}
             </Text>
+
+            <br></br>
+            Hecho con <span role="img" aria-label="amor"> ❤️ </span> por Ale
 
         </Box>
 
