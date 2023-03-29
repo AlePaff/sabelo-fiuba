716b85ecbdace5273379931753787410ff5c1f32
AlePaff <alejandropaff@gmail.com>
2023-03-29 01:47:55 -0300

    assets movidos a public

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -23,7 +23,9 @@ const onClick = () => {
 function CardSabelo({ nombre, desc, link, imagen, categoria, observacion, login_info }) {
     // const PLACEHOLDER = "images/placeholder.png";
     const PLACEHOLDER = "https://i.imgur.com/g374iQx.png";
-    const ROOT_IMGS = './src/assets/';
+    const ROOT_IMGS = './assets/';
+
+    // images/fiuba-drive.jpg
 
     //si no existe la imagen, se usa el placeholder
     imagen = (imagen === "") ? PLACEHOLDER : imagen
