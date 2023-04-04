1beca154d5d74d2a9d743991f13ede8304267e16
AlePaff <alejandropaff@gmail.com>
2023-04-03 22:03:49 -0300

    al actuliz. no se guarda info del icono si de like

--- src/components/InteractionMetrics.jsx
+++ src/components/InteractionMetrics.jsx
@@ -8,32 +8,51 @@ import { addDoc, collection, doc, getDoc, updateDoc, setDoc } from "firebase/fir
 
 function InteractionMetrics({ id_card }) {
     const [active, setActive] = useState(false);
+    const [likes, setLikes] = useState(0);
     
+    useEffect(() => {
+        const likedCards = JSON.parse(localStorage.getItem('likedCards') || '{}');
+        if (likedCards[id_card]) {
+            setActive(true);
+            setLikes(likedCards[id_card]);
+        }
+    }, [id_card]);
+
     const handleClick = async () => {
-        setActive(!active);
-      
+        if (active) {
+            setActive(false);
+            setLikes(likes - 1);
+        } else {
+            setActive(true);
+            setLikes(likes + 1);
+        }
+
         const cardRef = doc(db, 'cards', id_card);
         const cardDoc = await getDoc(cardRef);
-      
-        if (cardDoc.exists()) {
-          const cardData = cardDoc.data();
-          const likes = cardData.likes ? cardData.likes + 1 : 1;
-      
-          await updateDoc(cardRef, { likes });
-        } else {
-          
-            await setDoc(cardRef, { likes: 1 });
 
+        if (cardDoc.exists()) {
+            const cardData = cardDoc.data();
+            const newLikes = active ? cardData.likes - 1 : cardData.likes + 1;
+
+            await updateDoc(cardRef, { likes: newLikes });
+        } else {
+            await setDoc(cardRef, { likes: 1 });
         }
-      };
-      
+
+        const likedCards = JSON.parse(localStorage.getItem('likedCards') || '{}');
+        likedCards[id_card] = likes;
+        if (active) {
+            delete likedCards[id_card];
+        }
+        localStorage.setItem('likedCards', JSON.stringify(likedCards));
+    };
+
 
 
     return (
         <Flex style={{ marginLeft: 'auto' }}>
             <button
                 style={{ cursor: 'pointer' }}
-                // onClick={() => setActive(!active)}
                 onClick={handleClick}
             >
                 <Icon as={active ? AiFillHeart : AiOutlineHeart} w={7} h={7} color="red.400"
