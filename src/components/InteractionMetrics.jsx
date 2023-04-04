5175786670a42b39801e1e2b454f9557de4d8d0d
AlePaff <alejandropaff@gmail.com>
2023-04-03 23:26:14 -0300

    los likes persisten si la pagina es refrescada

--- src/components/InteractionMetrics.jsx
+++ src/components/InteractionMetrics.jsx
@@ -7,18 +7,38 @@ import { addDoc, collection, doc, getDoc, updateDoc, setDoc } from "firebase/fir
 
 
 function InteractionMetrics({ id_card }) {
-    const [active, setActive] = useState(false);
+    const [active, setActive] = useState(false)
     const [likes, setLikes] = useState(0);
-    
+    const [totalLikes, setTotalLikes] = useState(0);
+
     useEffect(() => {
-        const likedCards = JSON.parse(localStorage.getItem('likedCards') || '{}');
+        //obtiene los likes de la base de datos
+        const fetchLikes = async () => {
+            const cardRef = doc(db, 'cards', id_card);
+            const cardDoc = await getDoc(cardRef);
+
+            if (cardDoc.exists()) {
+                const cardData = cardDoc.data();
+                setTotalLikes(cardData.likes || 0);
+            }
+        };
+
+        fetchLikes();
+    }, [id_card]);
+
+    useEffect(() => {
+        //obtiene los likes del local storage para impedir que se pueda votar varias veces al refrescar la página
+        const likedCards = JSON.parse(sessionStorage.getItem('likedCards') || '{}');      //localStorage no expira en cambio sessionStorage cuando se resetea el navegador
         if (likedCards[id_card]) {
             setActive(true);
             setLikes(likedCards[id_card]);
         }
-    }, [id_card]);
+    }, [id_card]);      //cada vez que se actualiza el id_card
+
 
     const handleClick = async () => {
+        console.log("likes", likes, "totalLikes", totalLikes, "active", active, "local", JSON.parse(sessionStorage.getItem('likedCards') || '{}'));
+
         if (active) {
             setActive(false);
             setLikes(likes - 1);
@@ -35,27 +55,37 @@ function InteractionMetrics({ id_card }) {
             const newLikes = active ? cardData.likes - 1 : cardData.likes + 1;
 
             await updateDoc(cardRef, { likes: newLikes });
+            setTotalLikes(newLikes);
         } else {
             await setDoc(cardRef, { likes: 1 });
+            setTotalLikes(1);
         }
 
-        const likedCards = JSON.parse(localStorage.getItem('likedCards') || '{}');
-        likedCards[id_card] = likes;
+        const likedCards = JSON.parse(sessionStorage.getItem('likedCards') || '{}');
+        likedCards[id_card] = !active;
+        console.log("-----", "likedCards", likedCards, "id card", id_card);
         if (active) {
             delete likedCards[id_card];
         }
-        localStorage.setItem('likedCards', JSON.stringify(likedCards));
+        sessionStorage.setItem('likedCards', JSON.stringify(likedCards));
     };
 
 
 
     return (
         <Flex style={{ marginLeft: 'auto' }}>
+            {likes} |
+            {totalLikes} {totalLikes === 1 ? 'like' : 'likes'}
+
             <button
                 style={{ cursor: 'pointer' }}
                 onClick={handleClick}
             >
-                <Icon as={active ? AiFillHeart : AiOutlineHeart} w={7} h={7} color="red.400"
+                <Icon
+                    as={active ? AiFillHeart : AiOutlineHeart}
+                    w={7}
+                    h={7}
+                    color="red.400"
                     _hover={{ transform: "scale(1.2)", transition: "all 0.2s ease-in-out" }}
                 />
             </button>
